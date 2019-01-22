51ec158901eb3203dcfadecb5bbc15f91abf8a06
Zhi Ji <zhiji17@berkeley.edu>
2019-01-22 15:03:59 -0500

    update about me

--- js/zhiji.js
+++ js/zhiji.js
@@ -1,7 +1,7 @@
 $(function () {
     $('.img-mouse').on('mouseover', function (e) {
         var pdfArr = {
-            "i1": "b",
+            "i1": "EI2018_final.png",
             "i2": "Predicting_stock_price_trend_using machine_learning_approach_ACM_submit _publication.png",
             "i3": "GANs-face-completion.png"
         };
