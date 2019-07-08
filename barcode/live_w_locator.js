45dc0d2361c1de98081dfd017f346a1a7bacb4f1
ZUJ0007 <zuj0007@m-c02w51lcg8wl.jet.local>
2019-07-08 11:13:44 -0400

    allow same barcode multiple times

--- barcode/live_w_locator.js
+++ barcode/live_w_locator.js
@@ -298,15 +298,15 @@ $(function() {
             console.log(difference);
             var diff_second = difference.getSeconds();
             var diff_mili = difference.getMilliseconds();
-            if (App.lastResult !== code) {
-                alert("time for scanning is "+ diff_second+":"+diff_mili);
-                App.lastResult = code;
-                var $node = null, canvas = Quagga.canvas.dom.image;
-                $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
-                $node.find("img").attr("src", canvas.toDataURL());
-                $node.find("h4.code").html(code);
-                $("#result_strip ul.thumbnails").prepend($node);
-            }
+            // if (App.lastResult !== code) {
+            alert("time for scanning is "+ diff_second+":"+diff_mili);
+            App.lastResult = code;
+            var $node = null, canvas = Quagga.canvas.dom.image;
+            $node = $('<li><div class="thumbnail"><div class="imgWrapper"><img /></div><div class="caption"><h4 class="code"></h4></div></div></li>');
+            $node.find("img").attr("src", canvas.toDataURL());
+            $node.find("h4.code").html(code);
+            $("#result_strip ul.thumbnails").prepend($node);
+            // }
         }
 
     });
